d83132aac63d5db00eff061da71ee440102adc7a
richa688 <task.stadsolution@gmail.com>
2026-07-04 09:54:02 +0530

    first commit

---
+++ tests/testdemo.spec.js
@@ -0,0 +1,19 @@
+const {test,expect}=require('@playwright/test');
+
+test("Login fun",async({page}) =>{
+
+   await page.goto('https://www.automationexercise.com/login');
+
+   await page.fill('//*[@id="form"]/div/div/div[1]/div/form/input[2]','richa@gmail.com')
+
+   await page.fill('//*[@id="form"]/div/div/div[1]/div/form/input[3]','richa@123')
+
+   await page.click('//*[@id="form"]/div/div/div[1]/div/form/button');
+
+    
+});
+
+
+
+
+
