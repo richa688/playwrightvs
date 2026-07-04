ff552dd8f10c70f98096bc9df9c5a8bdfcac2e68
richa688 <task.stadsolution@gmail.com>
2026-07-04 10:03:07 +0530

    second_commit

--- tests/testdemo.spec.js
+++ tests/testdemo.spec.js
@@ -4,7 +4,7 @@ test("Login fun",async({page}) =>{
 
    await page.goto('https://www.automationexercise.com/login');
 
-   await page.fill('//*[@id="form"]/div/div/div[1]/div/form/input[2]','richa@gmail.com')
+   await page.fill('//*[@id="form"]/div/div/div[1]/div/form/input[2]','anuj@gmail.com')
 
    await page.fill('//*[@id="form"]/div/div/div[1]/div/form/input[3]','richa@123')
 
